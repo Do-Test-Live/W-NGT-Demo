0509e9bb69b918ea51668801cd1b522f5f191ca3
Monoget <monoget2@gmail.com>
2023-03-26 17:41:35 +0600

    update all

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,26 @@
+function activeClass(id) {
+    let collection = document.getElementsByClassName('all');
+    for (let i = 0; i < collection.length; i++) {
+        collection[i].setAttribute('style', 'display: none !important;')
+    }
+
+    collection = document.getElementsByClassName(id);
+    for (let i = 0; i < collection.length; i++) {
+        collection[i].setAttribute('style', 'display: flex !important;')
+    }
+
+    let tabLink = document.querySelectorAll(".ngt-nav-list li a");
+
+    tabLink.forEach(function () {
+        tabLink.forEach(function (item) {
+            item.classList.remove("active");
+        });
+    });
+
+    let element = document.getElementById(id);
+    element.classList.add("active");
+    console.log(id);
+}
+
+
 
