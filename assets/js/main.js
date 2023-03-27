c3071167504ddc08e80d2acb2be0acf968d58007
Monoget <monoget2@gmail.com>
2023-03-27 16:25:50 +0600

    update all

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,29 @@
+// disable right click
+document.addEventListener('contextmenu', event => event.preventDefault());
+
+document.onkeydown = function (e) {
+
+    // disable F12 key
+    if (e.keyCode == 123) {
+        return false;
+    }
+
+    // disable I key
+    if (e.ctrlKey && e.shiftKey && e.keyCode == 73) {
+        return false;
+    }
+
+    // disable J key
+    if (e.ctrlKey && e.shiftKey && e.keyCode == 74) {
+        return false;
+    }
+
+    // disable U key
+    if (e.ctrlKey && e.keyCode == 85) {
+        return false;
+    }
+}
+
 function activeClass(id) {
     let collection = document.getElementsByClassName('all');
     for (let i = 0; i < collection.length; i++) {
